bd82e30ad053df30ff59b98adf8f75da61a69a9f
unknown <peterataras@gmail.com>
2023-02-08 19:40:33 -0800

    seed data'

---
+++ Main/db/seeds.sql
@@ -0,0 +1,30 @@
+USE Trip_db;
+
+
+INSERT INTO Users (Username, Pass, Avatar)
+VALUES("John", TBA, "A1");
+INSERT INTO Users (Username, Pass, Avatar)
+VALUES("Peter", TBA, "A4");
+INSERT INTO Users (Username, Pass, Avatar)
+VALUES("Mariah", TBA, "A3");
+INSERT INTO Users (Username, Pass, Avatar)
+VALUES("Selma", TBA, "A2");
+
+INSERT INTO Trips (Departure, Trip_Name, Active)
+VALUES("1/1/2023", "Texas Trip", "No");
+INSERT INTO Trips (Departure, Trip_Name, Active)
+VALUES("2/1/2023", "Las Vegas B-Day", "yes");
+INSERT INTO Trips (Departure, Trip_Name, Active)
+VALUES("4/05/2022", "New York Meeting", "No");
+INSERT INTO Trips (Departure, Trip_Name, Active)
+VALUES("10/20/2022", "Mountaina Vacation", "No");
+
+INSERT INTO Markers (Lat/lon)
+VALUES('31.169621, -99.683617');
+INSERT INTO Markers (Lat/lon)
+VALUES('36.17191, -115.13997');
+INSERT INTO Markers (Lat/lon)
+VALUES('40.7648, -73.9808');
+INSERT INTO Markers (Lat/lon)
+VALUES("46.965260, -109.533691");
+
