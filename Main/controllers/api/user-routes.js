72e75de2086445162ae9c4a657f258f88ab74bfd
Sam Jensen <samual.jensen@gmail.com>
2023-02-09 21:08:32 -0800

    added create user, login, and logout routes for use-routes.js

--- Main/controllers/api/user-routes.js
+++ Main/controllers/api/user-routes.js
@@ -1,16 +1,63 @@
 const router = require('express').Router();
-const { request } = require('express');
-const { User }  = require('../../models');
+const { User } = require('../../models');
 
+// create new user
 router.post('/', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
+        const userData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// login 
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res
+                .status(400)
+                .json({ mesaage: 'Incorrect username or password, please try again.' });
+            return;
+        }
+        
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ mesaage: 'Incorrect username or password, please try again.' });
+            return;
+        }
 
         req.session.save(() => {
             req.session.user_id = userData.id;
-            req.session.logged_in = true;
+            req.session.loggedIn = true;
 
-            res.status
-        })
+            res.json({ user: userData, message: 'login succesful!' });
+        });
+    };
+});
+
+router.post('/logout', (req,res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
     }
 });
+
+module.exports = router;
+
