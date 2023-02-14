3ba0dff41f3de9fc26bae2bade1c70c422581cbb
Savannah Vel <savannah.vel@vivint.com>
2023-02-13 19:03:16 -0800

    added to controllers index.js

--- Main/controllers/index.js
+++ Main/controllers/index.js
@@ -4,10 +4,12 @@ const router = require('express').Router();
 // const sharedTripRoutes = require('./api/shared-trips-routes');
 const tripsRoutes = require('./api/trips-routes');
 // const userRoutes = require('./api/user-routes');
+const homeRoutes = require('./homeRoutes');
 
 // router.use('/markers', markerRoutes);
 // router.use('/shared-trips', sharedTripRoutes);
 router.use('/trips', tripsRoutes);
 // router.use('/user', userRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
