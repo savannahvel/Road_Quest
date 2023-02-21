e7cdd732909fac2353344d2eeb699e75729e3531
Sam Jensen <samual.jensen@gmail.com>
2023-02-20 19:01:04 -0800

    added query to trips-routes.js to pull marker data when looking at trips

--- Main/controllers/api/markers-routes.js
+++ Main/controllers/api/markers-routes.js
@@ -1,16 +1,82 @@
 const router = require('express').Router();
-const { Markers } = require('../../models');
+const { Markers, Trips, User } = require('../../models');
 // const withAuth = require('../../utils/auth');
 
 // find all markers
 router.get('/', async (req, res) => {
     try {
-        const markersData = await Markers.findAll();
+        const markersData = await Markers.findAll(
+            {
+            include: [
+                {
+                    model: Trips,
+                    attributes: ['id', 'departure', 'trip_name', 'is_active', 'is_shared'],
+                },
+            ]},
+        );
         res.status(200).json(markersData);
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+// find marker by id
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const markersData = await Markers.findOne({
+//             where: {
+//                 id: req.params.id,
+//             },
+//             include: [
+//                 {
+//                     model: Trips,
+//                     attributes: ['id', 'departure', 'trip_name', 'is_active', 'is_shared'],
+//                     include: {
+//                         model: User,
+//                         attributes: ['name']
+//                     }
+//                 },
+//             ]
+//         });
+
+//         if (!markersData) {
+//             res.status(404).json({ message: 'No marker found with that id' });
+//             return;
+//         }
+
+//         res.status(200).json(markersData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+// router.get('/:id', (req, res) => {
+//     Markers.findOne({
+//         attributes: [ 'id', 'location', 'trip_id'],
+//         include: [
+//             {
+//                 model: Trips,
+//                 attributes: [ 'id', 'departure', 'trip_name', 'is_active', 'is_shared'],
+//                 include: {
+//                     model: User,
+//                     attributes: [ 'name' ]
+//                 }
+//             },
+//             // {
+//             //     model: User,
+//             //     attributes: [ 'name' ]
+//             // }
+//         ]
+//     })
+//     .then(markersData => {
+//         const markers = markersData.map(markers => markers.get({ plain: true }));
+//         res.render('homepage', { markers, loggedIn: req.session.loggedIn });
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
 
 // create new marker
 router.post('/', async (req, res) => {
@@ -58,7 +124,7 @@ router.delete('/:id', async (req, res) => {
                 id: req.params.id,
             },
         });
-        
+
         if (!markersData) {
             res.status(404).json({ message: 'No marker found with that id' });
             return;
