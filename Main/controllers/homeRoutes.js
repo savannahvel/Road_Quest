f7be1a1d02822a5eab12d2b3c7b2ae9ce9a59d3b
poprit77 <115595317+poprit77@users.noreply.github.com>
2023-02-20 19:04:12 -0800

    Update homeRoutes.js

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -3,6 +3,17 @@ const { User, Trips, Markers } = require('../models');
 const sequelize = require('../config/connection');
 const exp = require('constants');
 
+//sign in page
+router.get('/', async (req, res) => {
+    // if (req.session.logged_in) {
+    //     res.redirect('/');
+    //     return;
+    // }
+    res.render('login', {
+        style: 'signin.css',
+        script: 'login.js'
+    })
+
 // redirects user to homepage once logged in
 router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
